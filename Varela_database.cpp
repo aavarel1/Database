5eb90255e49a8425f8b8d2ffdb580c65a40ae4ea
aavarel1 <112527875+aavarel1@users.noreply.github.com>
2025-03-03 04:02:34 -0600

    s

--- Varela_database.cpp
+++ Varela_database.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
-int main(){
-    cout << "hello world" << endl;
-    return 0;
+fstream file;
+string filename = "data.txt";
+struct Person {
+string id, firstName, lastName;
+int income;
+}; 
+
+int main() {
+Person p;
+file.open(filename);
+file >> p.id >> p.firstName >> p.lastName >> p.income;
+file.close();
+cout << "ID: " << p.id << endl;
+cout << "First name: " << p.firstName << endl;
+cout << "Last name: " << p.lastName << endl;
+cout << "Income: $" << p.income << endl;
+return 0;
 }
